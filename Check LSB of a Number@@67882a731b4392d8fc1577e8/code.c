f1b04d8fc0803bb12acdb0490b417894bb4f7996
1shone1 <shone0711.becse24@chitkara.edu.in>
2025-01-30 11:24:13 +0530

    Solution to Check LSB of a Number

--- Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -3,9 +3,9 @@ int main() {
     printf("Enter a number:");
     scanf(%d, &number);
     if (number & 1){
-        printf("The lsb of %d is 1(number is odd).\n", number);
+        printf("The lsb of %d is 1 (number is odd).\n", number);
   } else {
-    ("The lsb of %d is 0(number is even).\n", number);
+    ("The lsb of %d is 0 (number is even).\n", number);
   }
     return 0;
 }
